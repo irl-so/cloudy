bd45382d283cda63893df44268df1cece8bdfc7a
lifewinning <lifewinning@gmail.com>
2014-03-06 15:00:24 -0500

    "make it look like 1996"

--- src/script.js
+++ src/script.js
@@ -1,22 +1,59 @@
 //setting the map
-var map = L.mapbox.map('map', 'lifewinning.map-0lnszm21', {zoomControl: false})
-    .setView([38.9258, -77.2236], 11);
+var map = L.mapbox.map('map', 'llifewinning.heoomgdl', {zoomControl: false})
+    .setView([38.8932, -77.3249], 11);
 var ui = document.getElementById('map-ui');
 
 new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
 //now when we scroll down through text the map won't zoom all crazylike unless we tell it to
 //map.scrollWheelZoom.disable();
 
-var markerIcon = L.icon({
-			    iconURL:"src/marker.png",
-			    iconSize:     [20, 20], 
-			    iconAnchor:   [10,10], 
-			});
+//make lines connecting points in phone data geojson
+var latlngs = []
 
-L.geoJson(phone_data, {
-	onEachFeature: function (feature, layer){
-		icon: markerIcon;
-	}
+$.each(phone_data.features, function(index, feature){
+	var obj = {}
+	obj.lat = feature.properties.Lat
+	obj.lon = feature.properties.Lon
+	obj.time = feature.properties.Time
+	latlngs.push(obj)
+})
+
+//make sure that they're sorted by timestamp
+function compare(a,b) {
+  if (a.time < b.time)
+     return -1;
+  if (a.time > b.time)
+    return 1;
+  return 0;
+}
+
+latlngs.sort(compare);
+
+var pointList = []
+
+$.each(latlngs, function(index, llobj){
+	var point = new L.LatLng(llobj.lat, llobj.lon)
+	pointList.push(point)	
+})
+
+var firstpolyline = new L.Polyline(pointList, {
+color: 'white',
+weight: 3,
+opacity: 1,
+smoothFactor: 1
 }).addTo(map);
+
+//now add the phone data points as points
+L.geoJson(phone_data, 
+	{pointToLayer: function (feature, latlng) {
+		return L.circleMarker(latlng, {
+				radius: 6,
+				fillColor: "#CB7751",
+				color: "#fff",
+				weight: 1,
+				opacity: 1,
+				fillOpacity: 1});
+		}
+	}).addTo(map);
 
 var hash = new L.Hash(map);
